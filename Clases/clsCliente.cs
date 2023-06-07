5e8be9bd225b9df934771174a173d77adc64f80f
NiltonLopez <112464513+NiltonLopez@users.noreply.github.com>
2023-06-06 21:15:52 -0500

    Comentario De Prueba 2

--- Clases/clsCliente.cs
+++ Clases/clsCliente.cs
@@ -9,6 +9,7 @@ namespace pApp_Serv_WEB.Clases
     public class clsCliente
     {
         //Comentario de PRUEBAA
+        //Comentario de Prueba 2
         DBVentaRopaEntities dbVentaRopa = new DBVentaRopaEntities();
         public tblCliente Cliente = new tblCliente();
 
